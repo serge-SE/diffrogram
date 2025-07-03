b9fe7fe1c687b5f0e2fe4f61fd19a618d289f024
Serge Smirnoff <ssmirnoff@soundexpert.org>
2025-07-03 18:31:06 +0300

    Update diffrogram33.m
    The License update

--- diffrogram33.m
+++ diffrogram33.m
@@ -1,4 +1,5 @@
-% The code covered by FreeBSD License; see the footer for full text.
+% The code covered by BSD 2-Clause License.
+% Copyright (c) 2025, Serge Smirnoff (soundexpert.org)
 % Version 3.35; see the footer for ChangeLog.
 % The reference implementation of df-metric (Matlab 2014a)
 % Web: soundexpert.org/articles/-/blogs/visualization-of-distortion#part3
@@ -812,7 +813,7 @@ end
 return
 
 % ---------------------------- Changelog ----------------------------------
-
+%
 % V3.35
 % - Custom filtering of OUT with mg2020.mat was removed.
 % - now names of output images have REF file first, then OUT. WarpMargin
@@ -868,31 +869,3 @@ return
 % - direct colormap output without df and sp vectors
 % - added -Inf column (greyscale) to color map
 % - first public release
-
-% ------------------------- FreeBSD License -------------------------------
-%
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions are
-% met:
-%
-% 1. Redistributions of source code must retain the above copyright notice,
-% this list of conditions and the following disclaimer.
-% 2. Redistributions in binary form must reproduce the above copyright
-% notice, this list of conditions and the following disclaimer in the
-% documentation and/or other materials provided with the distribution.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-% IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-% CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-%
-% The views and conclusions contained in the software and documentation are
-% those of the authors and should not be interpreted as representing
-% official policies, either expressed or implied, of the FreeBSD Project.
